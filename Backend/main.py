a0efe23be1edae74bc85013bc1c105166044bfe9
Brechin <brechin@brechy.net>
2018-09-04 11:43:12 -0700

    doctest passing

--- Backend/main.py
+++ Backend/main.py
@@ -9,7 +9,12 @@ APP = Flask(__name__)
 def hello_world():
     """Return string Hello, World!
 
-    >>>hello_world()
+    >>> hello_world()
     'Hello, World!'
     """
+
     return 'Hello, World!'
+
+if __name__ == "__main__":
+    import doctest
+doctest.testmod()
