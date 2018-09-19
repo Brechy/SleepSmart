6e63d06d9dd7c4e9bd0802e67bedf6a1c473277b
Brechin <brechin@brechy.net>
2018-09-19 00:10:56 -0700

    MVP commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
-//playlists
-
+//stop button
+stopButton = document.querySelector('#stop')
+stopButton.addEventListener('click', () => {
+  setStatus('STOP');
+  console.log(`Status = STOP AUDIO (")(=^.^=)(") !`)
+})
 
 //play button
+//store status of playing in flask
+playButton = document.querySelector('#play')
+playButton.addEventListener('click', () => {
+  setStatus('PLAY');
+  console.log(`Status = PLAY AUDIO (")(=^.^=)(") !`)
+})
 
-//pause button
-
-//stop button
+function setStatus(status) {
+  axios.post('http://34.217.46.191:5000/status', {
+    status: status
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
